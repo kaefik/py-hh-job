a4facc221b546ed53245f860bac8f005ce6a0480
oilnur <oilnur@localhost.localdomain>
2018-04-14 22:07:30 +0300

    смена запуска браузера chrome в безоконном виде

--- hh_job_selenium.py
+++ hh_job_selenium.py
@@ -1,5 +1,7 @@
 import configparser
 from selenium import webdriver
+from selenium.common.exceptions import WebDriverException
+
 
 namefile_cfg='config.ini'
 
@@ -12,10 +14,17 @@ parol = config['settings']['password']
 
 user_agent = "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101"
 
+# настройка  Chrome
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+# set the window size
+options.add_argument('window-size=1200x600')
+
 # Open up a Firefox browser
-driver = webdriver.Firefox()
-#driver = webdriver.PhantomJS()
+#driver = webdriver.Firefox()
+driver = webdriver.Chrome(chrome_options=options)
 driver.get('https://kazan.hh.ru/account/login?backurl=%2F')
+
 
 elem_username = driver.find_elements_by_css_selector('input[name="username"]')
 print(elem_username)
@@ -29,11 +38,14 @@ elem_submit[0].click()
 
 driver.get('https://kazan.hh.ru/applicant/resumes')
 
-# find buttons refresh resume
-elem_but_refresh = driver.find_elements_by_css_selector('.bloko-icon-link')
-for el in elem_but_refresh:
-    el.click()
-
+try:
+    # find buttons refresh resume
+    elem_but_refresh = driver.find_elements_by_css_selector('.bloko-icon-link')
+    for el in elem_but_refresh:
+        el.click()    
+except WebDriverException:
+    print("Резюме обновить нельзя. попробуйте позже.")
+else:
+    print("Резюме обновлено!")
 
 driver.close()
-
